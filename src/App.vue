de27a3cb169256c2db9d347cd85fd0b1ad8ca6b5
Bilboking <brkbendreality@gmail.com>
2020-12-19 17:43:04 -0600

    working on Modal file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{title}}</h1>
+  <Modal />
+  <!--<input type="text" ref="name">
+  <button @click="handleClick">click me</button>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data(){
+    return {
+      title: 'My First Vue App'
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
 }
 </script>
@@ -23,4 +35,21 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+h1 {
+  border-bottom: Avenir, Helvetica, Arial, sans-serif;
+  display: inline-block;
+  padding-bottom: 10px;
+}
 </style>
+<!--npm run serve -->
+<!--https://github.com/Bilboking/Vue_SPA_project.git
+git remote set-url origin https://github.com/Bilboking/Vue_SPA_project.git
+git remote -v
+git add .
+git commit -m "Initial commit"
+git push origin -u master
+
+//git checkout -b "directory" or git branch testing <<- then ->> git push -u origin testing
+//git add . 
+//git commit -m "new branch start"
+//git push -u origin directory-->
